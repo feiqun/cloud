28f021027bfac68cfc2887cf877e0b0eeb1eed81
125690431@qq.com <125690431@qq.com>
2019-05-22 10:03:50 +0800

    提交

--- app/src/main/java/cn/com/feiqun/controller/HiController.java
+++ app/src/main/java/cn/com/feiqun/controller/HiController.java
@@ -1,6 +1,7 @@
 package cn.com.feiqun.controller;
 
 import cn.com.feiqun.feign.ServiceHi;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +18,12 @@ public class HiController {
     ServiceHi serviceHi;
 
     @GetMapping(value = "/hi")
+    @HystrixCommand(fallbackMethod = "isError")
     public String sayHi(@RequestParam String name) {
         return serviceHi.sayHiFromClientOne(name);
+    }
+
+    public String isError(String name) {
+        return "断路器生效";
     }
 }
--- service/src/main/java/cn/com/feiqun/controller/TestController.java
+++ service/src/main/java/cn/com/feiqun/controller/TestController.java
@@ -2,7 +2,6 @@ package cn.com.feiqun.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
